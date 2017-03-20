22b353d50ed8d308b8fedc924a8fa90531be2363
tobsirl <tobster111@gmail.com>
2017-03-20 16:33:36 +0000

    Added Validation on Address

--- api/contacts/contact.model.js
+++ api/contacts/contact.model.js
@@ -3,12 +3,23 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+
 var ContactSchema = new Schema({
   name: { type : String, required : true },
   address: String,
   age: { type: Number, min: 0, max: 120 },
   email: { type : String, required : true },
   updated: { type: Date, default: Date.now }
+
+  
+
 });
+
+ContactSchema.path('address').validate(function (v){
+  	if(v.length > 10 || v.length < 5) {
+  		return false
+		}
+		return true
+  }, 'Contact address should be between 5 and 10 characters');
 
 module.exports = mongoose.model('Contact', ContactSchema);
